b9d21fe13c47261bb7205d682b31279a5c31e601
szewczykmira <szewczyk.mira@gmail.com>
2014-11-01 19:41:58 +0100

    Association added

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ActiveRecord::Base
+  has_many :tickets
+  has_many :gigs, :through => :tickets
+  accepts_nested_attributes_for :tickets
 end
--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,2 +1,4 @@
 class Gig < ActiveRecord::Base
+  has_many :tickets
+  has_many :customers, :through => :tickets
 end
