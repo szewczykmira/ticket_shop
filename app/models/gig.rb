2d559bd77a6a5c75e991ff57cae25316b291bc6d
szewczykmira <szewczyk.mira@gmail.com>
2014-11-01 21:49:07 +0100

    Nice view added

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -2,4 +2,8 @@ class Gig < ActiveRecord::Base
   validates :date, :band, :price, uniqueness: true
   has_many :tickets
   has_many :customers, :through => :tickets
+
+  def name
+    "#{band} #{date}"
+  end
 end
